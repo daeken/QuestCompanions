2cb0931e6b2cfa706ee8d889d7d6408d75ad761c
Cody Brocious <cody.brocious@gmail.com>
2012-04-07 04:17:51 -0400

    Added user creation to admin interface.

--- handlers/admin.py
+++ handlers/admin.py
@@ -12,3 +12,18 @@ def get_news():
 @handler('admin/news_story', admin=True)
 def get_news(id):
 	return dict(story=News.one(id=int(id)))
+
+@handler('admin/users', admin=True)
+def get_user():
+	return dict(users=User.all())
+
+@handler('admin/user_create', admin=True)
+def get_user_create():
+	pass
+
+@handler(admin=True)
+def post_create_user(username, password, admin=False):
+	user = User.add(username, password, admin == u'on')
+	if user == None:
+		redirect(get_user_create)
+	redirect(get_user)
