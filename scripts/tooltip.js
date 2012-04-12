1a08c45de949edcecde9c5eb2e68f383835df1ce
Liam Davis <liam.davis@trezoid.com>
2012-04-12 13:54:43 +1000

    Fixed cross browser wackyness.

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -11,7 +11,7 @@ function handler(obj)
 {
  $(obj).focus( function() {
    $('.tip').remove();
-   $(obj).parent().append('<div class="tip" style="position:fixed; left:'+$(obj).offset().left + 'px; top: ' + ($(obj).offset().top + ($(obj).outerHeight()/ 2) ) + 'px; background: rgba(0,0,0,0.8); color: #fff; padding: 10px; border-radius:5px; z-index: 10;">' + $(obj).attr('data-hint') + '</div>');
+   $(obj).parent().append('<div class="tip" style="position:fixed; left:'+$(obj).offset().left + 'px; top: ' + ($(obj).offset().top + ($(obj).outerHeight(false)) ) + 'px; background: rgba(0,0,0,0.8); color: #fff; padding: 10px; border-radius:5px; z-index: 10;">' + $(obj).attr('data-hint') + '</div>');
     })
  $(obj).mouseout( function() {
    $('.tip').remove();
