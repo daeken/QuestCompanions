8333b86291e10b823237277fc968c55ad6698c14
Liam Davis <liam.davis@trezoid.com>
2012-04-12 13:22:36 +1000

    Tooltip hints on forms

---
+++ scripts/tooltip.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+ $('input').each(function() {
+   handler(this);
+  })
+ $('textarea').each(function() {
+   handler($(this));
+   })
+ });
+
+function handler(obj)
+{
+ $(obj).focus( function() {
+   $('.tip').remove();
+   $(obj).parent().append('<div class="tip" style="position:fixed; left:'+$(obj).offset().left + 'px; top: ' + ($(obj).offset().top + ($(obj).outerHeight()/ 2) ) + 'px; background: rgba(0,0,0,0.8); color: #fff; padding: 10px; border-radius:5px; z-index: 10;">' + $(obj).attr('data-hint') + '</div>');
+    })
+ $(obj).mouseout( function() {
+   $('.tip').remove();
+   })
+}
